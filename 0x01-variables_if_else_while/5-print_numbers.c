528a2c30eab09b7e5d09836d04be4e43d0077371
“adedamolaokunola” <adedamolaokunola01@gmail.com>
2022-09-09 22:21:48 -0700

    Variables

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * topic = Prints numbers from 0 to 9.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
